9619a78f2c0f8563251ef3a6ae0098c3f422fc8f
Treinamento <Treinamento@Lenovo-PC>
2020-03-10 18:47:04 -0300

    Construtor 1

---
+++ programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,20 @@
+package cursojava.executavel;
+
+import cursojava.classes.Aluno;
+
+public class PrimeiraClasseJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*Obejeto ainda não existe*/
+		
+		Aluno aluno1;
+	
+		/*Agora temos um objeto real na memória*/
+		Aluno aluno2 = new Aluno();
+		
+
+	}
+
+}
